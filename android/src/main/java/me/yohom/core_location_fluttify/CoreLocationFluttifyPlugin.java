cc9f35cbe7477a1b32f3744509d46ac60516182d
wood <13845554+wood911@users.noreply.github.com>
2025-09-16 16:46:03 +0800

    flutter v2 v2 embedding

--- android/src/main/java/me/yohom/core_location_fluttify/CoreLocationFluttifyPlugin.java
+++ android/src/main/java/me/yohom/core_location_fluttify/CoreLocationFluttifyPlugin.java
@@ -6,28 +6,25 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** CoreLocationFluttifyPlugin */
 public class CoreLocationFluttifyPlugin implements FlutterPlugin, MethodCallHandler {
+
+  private MethodChannel channel;
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "core_location_fluttify");
-    channel.setMethodCallHandler(new CoreLocationFluttifyPlugin());
+    // 创建 MethodChannel
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "core_location_fluttify");
+    channel.setMethodCallHandler(this);
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "core_location_fluttify");
-    channel.setMethodCallHandler(new CoreLocationFluttifyPlugin());
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
   }
 
   @Override
@@ -37,9 +34,5 @@ public class CoreLocationFluttifyPlugin implements FlutterPlugin, MethodCallHand
     } else {
       result.notImplemented();
     }
-  }
-
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
   }
 }
